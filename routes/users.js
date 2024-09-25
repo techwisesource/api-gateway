052b323d85867f30af500497c531359ed3e4896a
aryandh.a.w <aryandh.a.w@gmail.com>
2024-09-25 22:04:18 +0700

    integrasi login and implement jwt

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersHandler = require('./handler/user')
 
 router.post('/register', usersHandler.register);
+router.post('/login', usersHandler.login);
 
 module.exports = router;
