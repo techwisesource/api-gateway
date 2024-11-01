46313b6eaa39515cd48857f9213fe9a18add8684
aryandh.a.w <aryandh.a.w@gmail.com>
2024-11-01 15:56:16 +0700

    [service-course] bugfix routes

--- routes/courses.js
+++ routes/courses.js
@@ -5,6 +5,8 @@ const courseHandler = require('./handler/course');
 
 router.post("/", courseHandler.create);
 router.put("/:id", courseHandler.update);
-
+router.get("/", courseHandler.getAll);
+router.get("/:id", courseHandler.get);
+router.delete("/:id", courseHandler.destroy);
 
 module.exports = router;
