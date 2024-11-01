848e6e63e27912c5937038e8730babc5051be9bf
aryandh.a.w <aryandh.a.w@gmail.com>
2024-11-01 14:43:11 +0700

    [service-course] api update course

--- routes/courses.js
+++ routes/courses.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const courseHandler = require('./handler/course');
 
 router.post("/", courseHandler.create);
+router.put("/:id", courseHandler.update);
 
 
 module.exports = router;
