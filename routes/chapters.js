9bf0af2b7f5668dbed7a5e3dbae0aef347a4c3e9
aryandh.a.w <aryandh.a.w@gmail.com>
2024-11-02 10:55:56 +0700

    [service-course] api update chapter

--- routes/chapters.js
+++ routes/chapters.js
@@ -6,5 +6,6 @@ const chapterHandler = require('./handler/chapter');
 const verifyToken = require("../middlewares/verifyToken");
 
 router.post("/", chapterHandler.create);
+router.put("/:id", chapterHandler.update);
 
 module.exports = router;
